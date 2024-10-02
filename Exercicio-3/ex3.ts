b7fd4baf26817feabb2bd434a667b4229f5b57ee
Rafael Cruz <rcas2009@outlook.com>
2024-10-02 17:02:17 -0300

    Criando função para mostrar a bio ao clicar no botão

---
+++ Exercicio-3/ex3.ts
@@ -0,0 +1,17 @@
+function listarLista(){
+    const listaHtml = document.getElementById("lista");
+    if (listaHtml !== null){
+        listaHtml.textContent = "Lista: " + JSON.stringify(lista);
+    }
+    else {
+        console.log("Lista não existe")
+    }
+}
+
+function buttonBioById(){
+    const idPassado = document.getElementById("id") as HTMLInputElement;
+    const id = idPassado.value;
+
+    const resultadoElement = document.getElementById("resultadoLista") as HTMLElement;
+    resultadoElement.textContent = "Bio do ID: " + bioByIdImperativo(Number(id));
+}
