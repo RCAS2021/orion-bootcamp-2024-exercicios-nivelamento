e181b1851b1161fa976ed4e2b5c249cee9220e5c
Rafael Cruz <rcas2009@outlook.com>
2024-10-02 17:42:07 -0300

    Criando função para atualizar o nome ou a bio ao clicar no botão

--- Exercicio-3/ex3.ts
+++ Exercicio-3/ex3.ts
@@ -8,18 +8,43 @@ function listarLista(){
     }
 }
 
-function buttonBioById(){
+function getIdPassado(){
     const idPassado = document.getElementById("id") as HTMLInputElement;
     const id = idPassado.value;
+
+    return id;
+}
+
+function buttonBioById(){
+    const id = getIdPassado();
 
     const resultadoElement = document.getElementById("resultadoLista") as HTMLElement;
     resultadoElement.textContent = "Bio do ID: " + bioByIdImperativo(Number(id));
 }
 
 function buttonNameById(){
-    const idPassado = document.getElementById("id") as HTMLInputElement;
-    const id = idPassado.value;
+    const id = getIdPassado();
 
     const resultadoElement = document.getElementById("resultadoLista") as HTMLElement;
     resultadoElement.textContent = "Nome do ID: " + nameByIdImperativo(Number(id));
+}
+
+function buttonUpdateById(){
+    const id = getIdPassado();
+    const bioHtml = document.getElementById("bio") as HTMLInputElement;
+    const nomeHtml = document.getElementById("name") as HTMLInputElement;
+    const novaBio = bioHtml.value;
+    const novoNome = nomeHtml.value;
+
+    if (novaBio && !novoNome){
+        updateByIdImperativo(Number(id), undefined, novaBio);
+    }
+    else if (!novaBio && novoNome){
+        updateByIdImperativo(Number(id), novoNome, undefined);
+    }
+    else if (novoNome && novaBio){
+        updateByIdImperativo(Number(id), novoNome, novaBio);
+    }
+
+    listarLista();
 }
