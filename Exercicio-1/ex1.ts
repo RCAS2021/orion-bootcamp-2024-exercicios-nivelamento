ac85b3936d94f0cc2d7a2bae24a504c0d4724321
Rafael Cruz <rcas2009@outlook.com>
2024-10-03 23:37:39 -0300

    Corrigindo tratamentos de erro

--- Exercicio-1/ex1.ts
+++ Exercicio-1/ex1.ts
@@ -1,23 +1,39 @@
 // @ts-check
 
 function contarVogais(palavra?: string): number {
+
+
     const vogais = ['a', 'e', 'i', 'o', 'u'];
     let cont = 0;
+    
+    const resultadoElement = document.getElementById("resultado") as HTMLElement;
 
     if (!palavra) {
         const inputElement = document.getElementById("palavra") as HTMLInputElement;
-        palavra = inputElement.value;
+        if (inputElement && inputElement.value){
+            palavra = inputElement.value;
+        }
+        else {
+            console.log("Não foi fornecida nenhuma palavra.");
+            resultadoElement.textContent = "Não foi fornecida nenhuma palavra.";
+            return -1;
+        }
     }
 
     palavra = palavra.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-    for (let i of palavra.toLowerCase()) {
-        if (vogais.includes(i)) {
+    for (let caractere of palavra.toLowerCase()) {
+        if (vogais.includes(caractere)) {
             cont += 1;
         }
     }
 
-    const resultadoElement = document.getElementById("resultado") as HTMLElement;
-    resultadoElement.textContent = "Quantidade de vogais: " + cont;
+    if (cont < 1){
+        console.log("Não há vogais na palavra.")
+        resultadoElement.textContent = "Quantidade de vogais: " + cont + ", não há vogais na palavra.";
+    }
+    else {
+        resultadoElement.textContent = "Quantidade de vogais: " + cont;
+    }
+
     return cont;
 }
-console.log("Quantidade de vogais (console): " + contarVogais("testaaando"));
