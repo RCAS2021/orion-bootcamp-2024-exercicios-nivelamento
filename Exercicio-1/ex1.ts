5aa7b712f2b2d006d2b54e290a014c93ce8a10d0
Rafael Cruz <rcas2009@outlook.com>
2024-10-03 23:45:02 -0300

    Adicionando documentação no padrão TSDoc

--- Exercicio-1/ex1.ts
+++ Exercicio-1/ex1.ts
@@ -1,7 +1,15 @@
 // @ts-check
 
 function contarVogais(palavra?: string): number {
-
+    /**
+     * Retorna a quantidade de vogais, dada uma palavra através de input de formulário HTML ou chamada de função.
+     * 
+     * Se a palavra for passada como parâmetro na chamada da função, a contagem será feita a partir do parâmetro,
+     * se não, obtém o valor do campo input do formulário HTML, caso ambos estejam vazios, exibe uma mensagem de erro.
+     * 
+     * @param palavra - (Opcional) Palavra que terá suas vogais contadas, se não for fornecida durante a chamada de função, tentará capturar através do input do formulário HTML.
+     * @returns A quantidade de vogais na palavra do parâmetro palavra. Retorna -1 se não houver uma palavra fornecida e exibe uma mensagem de erro.
+     */
 
     const vogais = ['a', 'e', 'i', 'o', 'u'];
     let cont = 0;
